325da618b4c9456219c67dd884a3f63b047b1033
Muskan-771 <muskanvij771@gmail.com>
2026-09-13 12:53:04 +0000

    registeration logic

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> {
+    console.log('Connected to MongoDB')
+})
+.catch((error)=>{
+    console.log('Error connecting to MongoDB: ', error);
+})
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
